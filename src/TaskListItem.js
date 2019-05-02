392befbc269e797f322f486fb4c5bb45a6b12ba0
Rylan Bauermeister <apple@MacBook-Pro-4.local>
2019-05-02 14:21:45 -0700

    Done.

--- src/TaskListItem.js
+++ src/TaskListItem.js
@@ -1,3 +1,47 @@
 class TaskListItem {
-  
+
+  constructor(data){
+    this.id = data.id
+    this.description = data.description
+  }
+
+  render() {
+    const li = document.createElement('li')
+    li.textContent = this.description
+    const rmButton = document.createElement('button')
+    rmButton.textContent = " X "
+    rmButton.addEventListener('click', ()=>{
+      li.remove()
+      this.delete()
+    })
+
+    li.appendChild(rmButton)
+    return li
+  }
+
+  delete(){
+    fetch('http://localhost:3000/posts/' + this.id, {
+      method: "DELETE",
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+  }
+
+  save(){
+    fetch('http://localhost:3000/posts', {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        description: this.description
+      })
+    })
+    .then(res => res.json())
+    .then(json => {
+      this.id = json.id
+    })
+  }
+
 }
--- src/taskList.js
+++ src/taskList.js
@@ -1,3 +1,28 @@
 class TaskList {
-  
+
+  constructor(){
+    this.list = document.getElementById("list");
+  }
+
+  addNewPost(description){
+    const taskPost = new TaskListItem({description})
+    taskPost.save()
+    this.list.appendChild(taskPost.render())
+  }
+
+  render(){
+    fetch('http://localhost:3000/posts')
+    .then(res => res.json())
+    .then(json => this.addPosts(json))
+  }
+
+  addPosts(posts) {
+    posts.forEach(post => this.addPost(post))
+  }
+
+  addPost(post){
+    const item = new TaskListItem(post)
+    this.list.appendChild(item.render())
+  }
+
 }
